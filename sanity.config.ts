ae3b79a8e23227210e6c00026ddd231ad7b1fd19
xinguouo <xinguouo@gmail.com>
2026-08-04 18:26:57 +0800

    Enable delete action for art projects

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,35 +1,49 @@
-'use client'
+"use client";
 
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 
-import {apiVersion, dataset, projectId, studioBasePath} from './src/sanity/env'
-import {schemaTypes} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import {
+  apiVersion,
+  dataset,
+  projectId,
+  studioBasePath,
+} from "./src/sanity/env";
+import { schemaTypes } from "./src/sanity/schemaTypes";
+import { structure } from "./src/sanity/structure";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Qingjing Site',
+  name: "default",
+  title: "Qingjing Site",
   projectId,
   dataset,
   basePath: studioBasePath,
   apiVersion,
-  plugins: [structureTool({structure})],
+  plugins: [structureTool({ structure })],
   schema: {
     types: schemaTypes,
   },
   document: {
+    actions: (prev, context) => {
+      if (context.schemaType === "artProject") {
+        return prev;
+      }
+
+      return prev;
+    },
     newDocumentOptions: (prev) => {
       const singletonTemplateIds = new Set([
-        'siteSettings',
-        'homePage',
-        'aboutMissionPage',
-        'aboutContactPage',
-        'contactPage',
-        'residencyPage',
-      ])
+        "siteSettings",
+        "homePage",
+        "aboutMissionPage",
+        "aboutContactPage",
+        "contactPage",
+        "residencyPage",
+      ]);
 
-      return prev.filter((templateItem) => !singletonTemplateIds.has(templateItem.templateId))
+      return prev.filter(
+        (templateItem) => !singletonTemplateIds.has(templateItem.templateId),
+      );
     },
   },
-})
+});
